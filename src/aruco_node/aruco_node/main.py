7e3ff2a8be8cec68cd45d8c09c6cceb6579b38b0
Tyler R <tyler73750@gmail.com>
2025-02-07 11:02:08 -0600

    Got rid of video writing ability and started action server

--- src/aruco_node/aruco_node/main.py
+++ src/aruco_node/aruco_node/main.py
@@ -1,4 +1,5 @@
 import rclpy
+import rclpy.action import ActionServer
 import matplotlib.pyplot as plt
 from rclpy.node import Node
 import cv2 as cv
@@ -10,6 +11,7 @@ import numpy as np
 # NOTE: This may not be the most effective, we could turn the image into an JPEG string or even define a custom ROS data type.
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from interfaces.action import TrackArucoMarker
 
 # A map of aruco dictionary strings to opencv.aruco enum values
 aruco_dict_map = {
@@ -43,30 +45,6 @@ class ArucoNode(Node):
                 read_only=True
             )
         ).get_parameter_value().string_value
-        self.write_video = self.declare_parameter('write_video', False,
-            ParameterDescriptor(
-                description="If set to True, it will create all output videos in the `/{video_dir}` directory",
-                read_only=True
-            )
-        ).get_parameter_value().string_value
-        self.video_dir = self.declare_parameter('video_dir', 'videos', 
-           ParameterDescriptor(
-                description="The name of the directory where video files are saved",
-                read_only=True
-            )
-        ).get_parameter_value().string_value
-        self.video_fps = self.declare_parameter('video_fps', 30, 
-           ParameterDescriptor(
-                description="The frames per second (FPS) of the output videos",
-                read_only=True
-           )
-        ).get_parameter_value().integer_value
-        # TODO: You should be able to change this parameter while the node is running and everything work fine
-        self.ids_to_detect = self.declare_parameter('ids_to_detect', [1],
-           ParameterDescriptor(
-                description="The ids to detect in an image",
-           )
-        ).get_parameter_value().integer_array_value
         self.aruco_dict = self.declare_parameter('aruco_dict', "4x4_50", # correlates to URC standard dictionary
            ParameterDescriptor(
                 description="This is the aruco dictionary number to assume aruco tags are from which come from 'cv2.aruco' predefined dictionary enum",
@@ -85,15 +63,6 @@ class ArucoNode(Node):
          self.rep_error
         ) = self.read_camera_config_file()
         self.get_logger().info("Finished reading calibration information for camera")
-
-        # Video writer creation
-        self.write_video = self.get_parameter('write_video').get_parameter_value().bool_value
-        if self.write_video:
-            self.video_writer = None
-
-            os.makedirs(self.video_dir, exist_ok=True)
-            self.video_path = os.path.join(self.video_dir, "output_video.mp4")
-            self.get_logger().info("Finished creating video writer for tracking output")
 
         # Aruco detector configuration
         self.detector_params = aruco.DetectorParameters() # TODO: Look into this
@@ -118,6 +87,12 @@ class ArucoNode(Node):
         # Useful log information on startup
         self.get_logger().info(f"Set to detect the following aruco ids with marker length {self.marker_length}: {list(self.ids_to_detect)}")
 
+        self._action_server = ActionServer(
+            self,
+            TrackArucoMarker,
+            'track_aruco_marker',
+            self.image_callback)
+
 
     def read_camera_config_file(self):
         """Read the camera configuration file and return parameters"""
@@ -136,21 +111,10 @@ class ArucoNode(Node):
         return camera_mat, dist_coeffs, rep_error
 
     def image_callback(self, image_msg):
-        """Process images received from topic"""
+        """ Store the latest image for proccesing. """
         try:
             # Convert ROS2 Image to OpenCV format
             cv_image = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='bgr8')
-
-            # Initialize the video writer if activated
-            if self.write_video and self.video_writer is None:
-                frame_size = (cv_image.shape[1], cv_image.shape[0])
-                self.video_writer = cv.VideoWriter(
-                    self.video_path,
-                    cv.VideoWriter_fourcc(*'mp4v'),
-                    self.video_fps,
-                    self.frame_size
-                )
-                self.get_logger().info(f"Video recording started: {self.video_path}")
 
             # Detect the marker ids
             marker_corners, marker_ids = self.detect_aruco_markers(cv_image)
@@ -162,12 +126,15 @@ class ArucoNode(Node):
             # Calculate pose for each marker
             if (marker_ids is not None and len(marker_ids) != 0):
                 for img_points in marker_corners:
-                    retval, rvec, tvec = calculate_pose(img_points)
+                    retval, rvec, tvec = self.calculate_pose(img_points)
 
 
 
         except Exception as e:
             self.get_logger().error(f"Error processing image: {e}")
+
+    def tracking_callback(self, goal_handle):
+        pass
         
     def detect_aruco_markers(self, image):
         """Given an image, return detected aruco markers and rejected markers (candidates for aruco markers)"""
@@ -187,12 +154,6 @@ class ArucoNode(Node):
         )
         return retval, rvec, tvec
         
-    def close_video(self):
-        """Close the video writer if it was activated"""
-        if self.write_video:
-            self.video_writer.release()
-            self.get_logger().info(f"Video Saved: '{self.video_path}'")
-
 def main(args=None):
     """Handle spinning up and destroying a node"""
     rclpy.init(args=args)
@@ -202,9 +163,7 @@ def main(args=None):
         rclpy.spin(aruco_node)
     except KeyboardInterrupt:
         pass
-    finally: # Ensure that the video writer closes properly
-        aruco_node.close_video()
-
+    finally:
         # Destroy the node explicitly
         # (optional - otherwise it will be done automatically
         # when the garbage collector destroys the node object)
