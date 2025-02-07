4cf792d64312dd66ceb9cf7d8a018af763ab7a13
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2025-02-07 17:14:13 -0600

    fix: use correct venv in source script
    god bless `uv` for saying this, literally never woulda noticed.
    
    like actually would've been several hours without uv. shoutout ros 2 >:(

--- SOURCE_SCRIPT.fish
+++ SOURCE_SCRIPT.fish
@@ -2,15 +2,13 @@
 
 echo -n "You should be running this script using `" && set_color green && echo -n ". ./SOURCE_SCRIPT.fish" && set_color normal && echo "`."
 echo "If you didn't do so, please restart the script."
-echo 
+echo
 
 echo Sourcing virtual environment...
-. src/log_node/.venv/bin/activate.fish
+. .venv/bin/activate.fish
 echo Done!
 
 echo Sourcing ROS 2 environment files...
 bass source install/local_setup.bash
 bass source /opt/ros/humble/setup.bash
 echo Done!
-
-
