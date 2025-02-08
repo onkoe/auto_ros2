9b913d6fc79a08cc718cad7a46ead83f11b6ff44
barrett <onkoe@protonmail.com>
2025-02-08 13:56:17 -0600

    feat(wheels): use ros 2 subscriber

--- src/wheels_node/src/main.rs
+++ src/wheels_node/src/main.rs
@@ -1,10 +1,12 @@
-use feedback::{Wheels, prelude::RoverController, parse::Message};
-use tokio::sync::{mpsc, Mutex};
-use std::{sync::Arc, net::{Ipv4Addr, IpAddr}};
+use std::net::{IpAddr, Ipv4Addr};
+
+use feedback::{parse::Message, prelude::RoverController, Wheels};
 use ros2_client::{
     log::LogLevel, ros2::QosPolicyBuilder, rosout, Context, MessageTypeName, Name, Node, NodeName,
-    NodeOptions, Subscription
+    NodeOptions, Subscription,
 };
+
+use tokio_stream::StreamExt as _;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
@@ -19,76 +21,93 @@ async fn main() {
     let ros2_context = Context::new().expect("init ros 2 context");
 
     // create the wheels node
-    let node = Arc::new(Mutex::new(create_node(&ros2_context)));
-    rosout!(node.lock().await, LogLevel::Info, "wheels node is online!");
+    let mut node = create_node(&ros2_context);
+    rosout!(node, LogLevel::Info, "wheels node is online!");
 
-    // make sthe wheels topic
-    let wheels_topic = {
-        let node_locked = node.lock().await;
-        let topic = node_locked.create_topic(
+    // make the wheels topic
+    let wheels_topic = node
+        .create_topic(
             &Name::new("/rover", "wheels").unwrap(),
             MessageTypeName::new("std_msgs", "String"),
             &ros2_client::DEFAULT_SUBSCRIPTION_QOS,
-        ).expect("create wheels topic");
-
-        topic
-    };
+        )
+        .expect("create wheels topic");
 
     // message channel to handle incoming data
-    // tx forwards messages recieved by topic
-    // rx processes messages on arrival
-    let (tx, rx) = mpsc::unbounded_channel::<String>();
-    let rx = Arc::new(tokio::sync::Mutex::new(rx));
-    // subscribe to wheels topic and forward messages to the channel
-    let _subscriber: Subscription<String> = {
-        let mut node_locked = node.lock().await;
-        node_locked.create_subscription(
-            &wheels_topic, 
-            Some(qos()) // Apply the Quality of Service settings.
-        ).expect("create subscription")
-    };
 
-    // ensures messges are processed correctly before being forwarded
-    // prevents ownership/borrow conflicts with rx
-    // : tokio::sync::mpsc::UnboundedReceiver<String>
-    let rx_receiver = Arc::clone(&rx);
-    tokio::task::spawn(async move {
-        while let Some(msg) = rx_receiver.lock().await.recv().await { // blocking call to receive messages
-            let _ = tx.send(msg).expect("Failed to send message"); // forward message to the channel
-        }
-    });
+    // subscribe to wheels topic and forward messages to the channel
+    //
+    // FIXME: use dedicated message type
+    let subscriber: Subscription<String> = node
+        .create_subscription(
+            &wheels_topic,
+            Some(qos()), // Apply the Quality of Service settings.
+        )
+        .expect("create subscription");
 
     // microcontroller that is responsible for controlling wheels
     // needs the microcontrollers' ip address and port
     let (ip, port): (Ipv4Addr, u16) = (Ipv4Addr::new(192, 168, 1, 102), 1002);
-    let controller = RoverController::new(IpAddr::V4(ip), port).await.expect("Failed to create RoverController");
+    let controller = RoverController::new(IpAddr::V4(ip), port, 0)
+        .await
+        .expect("Failed to create RoverController");
 
     // processes the recieved commands and sends the commands to the microcontroller
-    let rx_sendwheels = Arc::clone(&rx);
-    let node_clone = Arc::clone(&node);
+    let node_handle = node.logging_handle();
     let join_handle = tokio::task::spawn(async move {
-        while let Some(msg) = rx_sendwheels.lock().await.recv().await {
-            match parse_wheels_msg(&msg) { // parses the message into the correct data
-                Some(wheels) => {
-                    // if the message is valid, send the parsed wheel commands to the microcontroller.
-                    if let Err(e) = controller.send_wheels(&wheels).await {
-                        let node_locked = node_clone.lock().await;
-                        rosout!(node_locked, LogLevel::Error, "Failed to send wheels command: {e}");
-                    }
+        let mut stream = Box::pin(subscriber.async_stream());
+
+        while let Some(msg_result) = stream.next().await {
+            // get the data from it or log an err!
+            let (msg, _msg_info) = match msg_result {
+                Ok(t) => t,
+                Err(e) => {
+                    rosout!(
+                        node_handle,
+                        LogLevel::Warn,
+                        "Failed to parse message data! err: {e}"
+                    );
+                    continue;
                 }
-                None => {
-                    // log a warning if the message format is incorrect.
-                    let node_locked = node_clone.lock().await;
-                    rosout!(node_locked, LogLevel::Warn, "Received invalid wheels message: {msg}");
+            };
+
+            // parse the message into something we can send to the microcontroller.
+            //
+            // FIXME: use message type instead. no need to parse here.
+            let wheels = match feedback::parse::parse(msg.as_bytes()) {
+                Ok(Message::Wheels(wheels)) => wheels,
+                Ok(other) => {
+                    rosout!(
+                        node_handle,
+                        LogLevel::Error,
+                        "other msg recvd. todo we're not parsing a str. {other:?}"
+                    );
+                    continue;
                 }
+                Err(e) => {
+                    rosout!(
+                        node_handle,
+                        LogLevel::Error,
+                        "Failed to parse. TODO we're not parsing a str {e}"
+                    );
+                    continue;
+                }
+            };
+
+            // try sending it
+            if let Err(e) = controller.send_wheels(&wheels).await {
+                rosout!(
+                    node_handle,
+                    LogLevel::Error,
+                    "Failed to send wheels command: {e}"
+                );
             }
         }
     });
 
     // make the node do stuff
     {
-    let mut node_locked = node.lock().await;
-    spin(&mut node_locked);
+        spin(&mut node);
     }
 
     if let Err(e) = join_handle.await {
@@ -148,23 +167,4 @@ fn spin(node: &mut Node) {
                 .inspect_err(|e| tracing::warn!("failed to spin node! see: {e}"));
         });
     }
-}
-
-// converts the incoming message into the correct byte array based on feedback
-#[tracing::instrument]
-fn parse_wheels_msg(msg: &str) -> Option<Wheels> {
-	let bytes = msg.as_bytes();
-
-	let parsed_msg_res = feedback::parse::parse(bytes)
-		.inspect_err(|e| tracing::debug!("Couldn't parse message! err: {e}"))
-		.ok();
-
-	if let Some(parsed_msg) = parsed_msg_res {
-		if let Message::Wheels(wheels) = parsed_msg {
-			tracing::debug!("Got a wheels message! {wheels:#?}");
-			return Some(wheels);
-		}
-	}
-
-	None
 }
