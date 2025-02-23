035eb9d423f60688935d80f84a3953121ef2b529
Barrett Ray <contact@barretts.club>
2025-02-22 23:03:06 -0600

    feat(sim): add navigator to sim launch file

--- src/soro_simulator_driver/launch/launch_sim.py
+++ src/soro_simulator_driver/launch/launch_sim.py
@@ -1,11 +1,13 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 from webots_ros2_driver.webots_controller import WebotsController
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 
 import launch
 from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 
 
@@ -24,17 +26,28 @@ def generate_launch_description():
         ],
     )
 
+    # stop everything when webots goes down
+    exit_handler: RegisterEventHandler = launch.actions.RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=webots,
+            on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+        )
+    )
+
+    # make an instance of the navigator node!
+    navigator: Node = Node(
+        executable="navigator_node",
+        package="navigator_node",
+        name="navigator",
+        # parameters=[
+        #     {
+        #         "navigator_parameters": LaunchConfiguration(
+        #             "navigator_parameters"
+        #         )
+        #     }
+        # ],
+    )
+
     return LaunchDescription(
-        [
-            webots,
-            my_robot_driver,
-            launch.actions.RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=webots,
-                    on_exit=[
-                        launch.actions.EmitEvent(event=launch.events.Shutdown())
-                    ],
-                )
-            ),
-        ]
+        [webots, my_robot_driver, exit_handler, navigator],
     )
