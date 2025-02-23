d79ea53bf6e8675b70f3cabfb1e782b4a7d66ef6
Barrett Ray <contact@barretts.club>
2025-02-22 22:35:25 -0600

    fix(sim): modify `setup.py` to be... correct
    woops. forgot u had to do this. we should probably add this to the docs
    lol

--- src/soro_simulator_driver/setup.py
+++ src/soro_simulator_driver/setup.py
@@ -1,19 +1,28 @@
 from setuptools import find_packages, setup
 
 package_name = "soro_simulator_driver"
+data_files = []
+data_files.append(
+    ("share/ament_index/resource_index/packages", ["resource/" + package_name])
+)
+data_files.append(
+    ("share/" + package_name + "/launch", ["launch/launch_sim.py"])
+)
+data_files.append(
+    ("share/" + package_name + "/worlds", ["worlds/base_world.wbt"])
+)
+data_files.append(
+    ("share/" + package_name + "/resource", ["resource/remi.urdf"])
+)
+data_files.append(("share/" + package_name, ["package.xml"]))
 
-setup(
+
+_ = setup(
     name=package_name,
     version="0.0.0",
     python_requires=">=3.13",
     packages=find_packages(exclude=["test"]),
-    data_files=[
-        (
-            "share/ament_index/resource_index/packages",
-            ["resource/" + package_name],
-        ),
-        ("share/" + package_name, ["package.xml"]),
-    ],
+    data_files=data_files,
     zip_safe=True,
     maintainer="brendan",
     maintainer_email="bford@axmilius.com",
@@ -21,7 +30,7 @@ setup(
     license="TODO",
     entry_points={
         "console_scripts": [
-            "log_node = log_node.main:main",
+            "driver = driver.main:main",
         ],
     },
 )
