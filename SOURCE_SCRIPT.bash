b187e076e29ee0d83770f4bc24e8a4bede2b36b5
barrett <onkoe@protonmail.com>
2025-04-19 17:42:11 -0500

    fix: source_script.bash now escapes backticks
    you don't need to do this in fish, and this was copy-pasted from fish in
    a hurry lol

--- SOURCE_SCRIPT.bash
+++ SOURCE_SCRIPT.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -n "You should be running this script using \`" && echo -n ". ./SOURCE_SCRIPT.fish" && echo "\`."
+echo -n "You should be running this script using \`" && echo -n ". ./SOURCE_SCRIPT.bash" && echo "\`."
 echo "If you didn't do so, please restart the script."
 echo
 
