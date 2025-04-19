5b1d042cb03aa428a2454a47a2cec8685b9437b1
barrett <onkoe@protonmail.com>
2025-04-19 15:01:47 -0500

    feat(docker): support podman

--- docker/scripts/fix_selinux.bash
+++ docker/scripts/fix_selinux.bash
@@ -7,7 +7,7 @@ set -e
 #
 # so, we'll start off by checking if we've got SELinux support using this test,
 # which only checks if the `selinuxenabled` command is present.
-if [ command -v selinuxenabled &> /dev/null ]; then
+if command -v selinuxenabled &> /dev/null; then
 
     # now, we'll run the command to see if SELinux is turned on.
     SELINUX_IS_ON=$(selinuxenabled)
@@ -15,11 +15,11 @@ if [ command -v selinuxenabled &> /dev/null ]; then
     # if this is `0`, it's on!
     #
     # that means we'll need to check if our repo is protected.
-    if [ "$depth" -eq "0" ]; then
+    if [ "${depth:-0}" -eq "0" ]; then
 
         # check if the repo is protected...
-        if [ ! /usr/bin/ls -Zd "$AUTO_ROS2_REPO_LOCATION" | grep -q "container_file_t" ]; then
-            # it isn't. let's prompt the user to fix it...
+        if ! /usr/bin/ls -Zd "$AUTO_ROS2_REPO_LOCATION" | grep -q "container_file_t"; then
+        # it isn't. let's prompt the user to fix it...
             echo "The \`auto_ros2\` repo is protected by SELinux. To mount it, we'll need to add an exception..."
             echo "Please enter your password if prompted."
             sudo chcon --recursive --type svirt_sandbox_file_t "$AUTO_ROS2_REPO_LOCATION"
@@ -27,3 +27,5 @@ if [ command -v selinuxenabled &> /dev/null ]; then
         fi
     fi
 fi
+
+echo "test"
