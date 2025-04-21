1c891d4e584db43e26492620eba50b734017d26f
barrett <onkoe@protonmail.com>
2025-04-21 16:12:54 -0500

    feat(sim/launch): add roverless sim launch
    for testing

--- src/simulator/setup.py
+++ src/simulator/setup.py
@@ -14,6 +14,7 @@ _ = setup(
         ),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", ["launch/sim.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/gazebo_only.launch.py"]),
         ("share/" + package_name + "/resource", ["resource/world.sdf.xml"]),
         (
             "share/" + package_name + "/resource",
