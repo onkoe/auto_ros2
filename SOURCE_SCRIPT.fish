f617b3f7f592b76379dfbf1c7b6f82015f6b976d
barrett <onkoe@protonmail.com>
2025-04-26 17:43:10 -0500

    feat: update SOURCE_SCRIPTs to respect .ros_**

--- SOURCE_SCRIPT.fish
+++ SOURCE_SCRIPT.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
 
+set ROS2_WORKSPACE_LOCATION (dirname (readlink -m (status --current-filename)))
+
+# grab ros 2 properties
+set ROS_DISTRO (cat .ros_distro)
+set ROS_PATH (cat .ros_path)$ROS_DISTRO
+
 echo -n "You should be running this script using `" && set_color green && echo -n ". ./SOURCE_SCRIPT.fish" && set_color normal && echo "`."
 echo "If you didn't do so, please restart the script."
 echo
@@ -9,6 +15,6 @@ echo Sourcing virtual environment...
 echo Done!
 
 echo Sourcing ROS 2 environment files...
-bass source install/local_setup.bash
-bass source /opt/ros/humble/setup.bash
+bass source $ROS2_WORKSPACE_LOCATION/install/local_setup.bash || true
+bass source $ROS_PATH/setup.bash || true
 echo Done!
