3281ca8733758ec24dc8f7bb74ab2dc055554637
barrett <onkoe@protonmail.com>
2025-04-29 02:22:10 -0500

    feat: use CycloneDDS by default
    recommended for nav2

--- SOURCE_SCRIPT.bash
+++ SOURCE_SCRIPT.bash
@@ -12,12 +12,25 @@ echo -n "You should be running this script using \`" && echo -n "$GREEN. ./SOURC
 echo "If you didn't do so, please restart the script."
 echo
 
-echo Sourcing virtual environment...
+echo "Sourcing virtual environment..."
 . .venv/bin/activate
-echo Done!
+echo "Done!"
 echo
 
-echo Sourcing ROS 2 environment files...
+echo "Sourcing ROS 2 environment files..."
 . install/local_setup.bash || true
 . $ROS_PATH/setup.bash || true
-echo Done!
+echo "Done!"
+echo
+
+echo "Setting Nav2-compatible ROS middleware implementation..."
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+echo "Done!"
+echo
+
+echo "Turning on colorized output from ROS 2..."
+export RCUTILS_COLORIZED_OUTPUT=1
+echo "Done!"
+echo
+
+echo "All source tasks are now complete!"
--- SOURCE_SCRIPT.fish
+++ SOURCE_SCRIPT.fish
@@ -10,11 +10,25 @@ echo -n "You should be running this script using `" && set_color green && echo -
 echo "If you didn't do so, please restart the script."
 echo
 
-echo Sourcing virtual environment...
+echo "Sourcing virtual environment..."
 . .venv/bin/activate.fish
-echo Done!
+echo "Done!"
+echo
 
-echo Sourcing ROS 2 environment files...
+echo "Sourcing ROS 2 environment files..."
 bass source $ROS2_WORKSPACE_LOCATION/install/local_setup.bash || true
 bass source $ROS_PATH/setup.bash || true
-echo Done!
+echo "Done!"
+echo
+
+echo "Setting Nav2-compatible ROS middleware implementation..."
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+echo "Done!"
+echo
+
+echo "Turning on colorized output from ROS 2..."
+export RCUTILS_COLORIZED_OUTPUT=1
+echo "Done!"
+echo
+
+echo "All source tasks are now complete!"
