31d2e772072060416a2ad55e6a692a071b7bd541
Barrett Ray <contact@barretts.club>
2025-04-30 17:26:03 -0500

    fix(drive/launch/slam_tb): camera frame mismatch
    in `slam_toolbox`, we were getting an error whenever we got any info
    from the depth camera. `slam_toolbox` would say that it was
    `dropping message` as the `queue is full`.
    
    these errors would repeat over and over:
    
    ```console
    [async_slam_toolbox_node-12] [INFO] [1745986106.657721647]
    [slam_toolbox]: Message Filter dropping message: frame
    'camera_depth_frame' at time 14.000 for reason 'discarding message
    because the queue is full'
    [async_slam_toolbox_node-12] [INFO] [1745986106.762131629]
    [slam_toolbox]: Message Filter dropping message: frame
    'camera_depth_frame' at time 14.100 for reason 'discarding message
    because the queue is full'
    [async_slam_toolbox_node-12] [INFO] [1745986106.861317526]
    [slam_toolbox]: Message Filter dropping message: frame
    'camera_depth_frame' at time 14.200 for reason 'discarding message
    because the queue is full'
    [async_slam_toolbox_node-12] [INFO] [1745986106.963220456]
    [slam_toolbox]: Message Filter dropping message: frame
    'camera_depth_frame' at time 14.300 for reason 'discarding message
    because the queue is full'
    ```
    
    this problem stemmed from the lack of a link between `camera_link` and
    the `camera_depth_frame`. we need those to be connected or they can't
    talk about one another.
    
    (you can tell with `ros2 run rqt_tf_tree rqt_tf_tree`...)
    
    anyway, to solve it, we create a node that makes that a chain:
    
    ```python
    def _make_camera_depth_frame_link_node(
        use_sim_time: LaunchConfiguration,
    ) -> Node:
        return Node(
            name="tf2_publish_camera_depth_frame_link_node",
            package="tf2_ros",
            executable="static_transform_publisher",
            arguments=[
                "0",  # x
                "0",  # y
                "0",  # z
                "0",  # rx
                "0",  # ry
                "0",  # rz
                "camera_link",  # parent frame
                "camera_depth_frame",  # subframe
            ],
            parameters=[{"use_sim_time": use_sim_time}],
        )
    ```
    
    then, we spawn it with the `slam_toolbox` node:
    
    ```python
        return LaunchDescription(
            [
                DeclareLaunchArgument(
                    "use_sim_time",
                ),
                camera_chain,
                slam_toolbox,
            ]
        )
    ```
    
    that resolves those problems!

--- src/drive_launcher/launch/helpers/slam_toolbox.launch.py
+++ src/drive_launcher/launch/helpers/slam_toolbox.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -14,6 +15,10 @@ def generate_launch_description() -> LaunchDescription:
     use_sim_time: LaunchConfiguration = LaunchConfiguration(
         "use_sim_time", default="false"
     )
+
+    # we need this little node to make a link between `camera_link` and
+    # `camera_depth_frame`
+    camera_chain: Node = _make_camera_depth_frame_link_node(use_sim_time)
 
     slam_toolbox = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -39,6 +44,7 @@ def generate_launch_description() -> LaunchDescription:
             DeclareLaunchArgument(
                 "use_sim_time",
             ),
+            camera_chain,
             slam_toolbox,
         ]
     )
@@ -51,4 +57,25 @@ def _get_conf() -> PathJoinSubstitution:
             "params",
             "slam_toolbox.yaml",
         ]
+    )
+
+
+def _make_camera_depth_frame_link_node(
+    use_sim_time: LaunchConfiguration,
+) -> Node:
+    return Node(
+        name="tf2_publish_camera_depth_frame_link_node",
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[
+            "0",  # x
+            "0",  # y
+            "0",  # z
+            "0",  # rx
+            "0",  # ry
+            "0",  # rz
+            "camera_link",  # parent frame
+            "camera_depth_frame",  # subframe
+        ],
+        parameters=[{"use_sim_time": use_sim_time}],
     )
