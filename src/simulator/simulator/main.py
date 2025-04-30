d9a87bfee0273fade765578e081b1c1d2f70b8a6
Barrett Ray <contact@barretts.club>
2025-04-30 17:37:03 -0500

    chore(sim/main): remove all the wheels stuff
    it's handled by `ros2_control` now...

--- src/simulator/simulator/main.py
+++ src/simulator/simulator/main.py
@@ -1,28 +1,21 @@
 import signal
 import sys
 from dataclasses import dataclass
-from time import sleep
 
 import rclpy
 from loguru import logger as llogger
 from rclpy.node import Node
-from rclpy.publisher import Publisher
 from rclpy.qos import (
     QoSPresetProfiles,
     QoSProfile,
 )
 from rclpy.service import Service
-from rclpy.subscription import Subscription
 from rclpy.utilities import try_shutdown
-from std_msgs.msg import Float64
 from typing_extensions import override
 
-from custom_interfaces.msg._wheels_message import WheelsMessage
 from custom_interfaces.srv._lights import Lights
 from custom_interfaces.srv._lights import Lights_Request as LightsRequest
 from custom_interfaces.srv._lights import Lights_Response as LightsResponse
-
-from .wheel_speeds import translate_u8
 
 QOS_PROFILE: QoSProfile = QoSPresetProfiles.SYSTEM_DEFAULT.value
 
@@ -31,66 +24,16 @@ QOS_PROFILE: QoSProfile = QoSPresetProfiles.SYSTEM_DEFAULT.value
 class SoroBridge(Node):
     """Translates stuff from the simulator into the messages we use."""
 
-    # motors... there are quite a few.
-    #
-    # these are directly from gazebo
-    __left_front_wheel_motor: Publisher
-    __left_middle_wheel_motor: Publisher
-    __left_back_wheel_motor: Publisher
-    __right_front_wheel_motor: Publisher
-    __right_middle_wheel_motor: Publisher
-    __right_back_wheel_motor: Publisher
-
     # interface speakers/listeners
     __lights_service: Service
-    __wheels_subscriber: Subscription
-
-    # TODO: remove old wheels subscriber
-    __left_wheels_subscriber: Subscription
-    __right_wheels_subscriber: Subscription
-
-    # we'll publish the messages we get back onto these
-    __left_wheels_publisher: Publisher
-    __right_wheels_publisher: Publisher
 
     def __init__(self):
         # initialize the `Robot` superclass
         super().__init__("soro_bridge")
 
         # react to controls
-        self.__wheels_subscriber = self.create_subscription(
-            WheelsMessage, "/control/wheels", self.wheels_callback, QOS_PROFILE
-        )
         self.__lights_service = self.create_service(
             Lights, "/control/lights", self.lights_callback
-        )
-
-        # make wheel publishers
-        self.__left_front_wheel_motor = self.create_publisher(
-            Float64, "/sim/left_front_wheel/vel", QOS_PROFILE
-        )
-        self.__left_middle_wheel_motor = self.create_publisher(
-            Float64, "/sim/left_middle_wheel/vel", QOS_PROFILE
-        )
-        self.__left_back_wheel_motor = self.create_publisher(
-            Float64, "/sim/left_back_wheel/vel", QOS_PROFILE
-        )
-        self.__right_front_wheel_motor = self.create_publisher(
-            Float64, "/sim/right_front_wheel/vel", QOS_PROFILE
-        )
-        self.__right_middle_wheel_motor = self.create_publisher(
-            Float64, "/sim/right_middle_wheel/vel", QOS_PROFILE
-        )
-        self.__right_back_wheel_motor = self.create_publisher(
-            Float64, "/sim/right_back_wheel/vel", QOS_PROFILE
-        )
-
-        # new wheels subscribers
-        self.__left_wheels_publisher = self.create_publisher(
-            Float64, "/sim/right_back_wheel/vel", QOS_PROFILE
-        )
-        self.__right_wheels_publisher = self.create_publisher(
-            Float64, "/sim/right_back_wheel/vel", QOS_PROFILE
         )
 
     @override
@@ -110,41 +53,6 @@ class SoroBridge(Node):
         # self.__led.set(lights_value) # FIXME
 
         return resp  # FIXME: add fields
-
-    def wheels_callback(self, msg: WheelsMessage):
-        BASE_SPEED: float = 0.2
-        left_wheel_speed: float = BASE_SPEED * translate_u8(msg.left_wheels)
-        right_wheel_speed: float = BASE_SPEED * translate_u8(msg.right_wheels)
-
-        # make float64 msgs
-        left_msg: Float64 = Float64()
-        left_msg.data = left_wheel_speed
-        right_msg: Float64 = Float64()
-        right_msg.data = right_wheel_speed
-
-        # don't send wheel speeds before we have subscribers
-        while (
-            self.__left_back_wheel_motor.get_subscription_count() == 0
-            or self.__left_middle_wheel_motor.get_subscription_count() == 0
-            or self.__left_front_wheel_motor.get_subscription_count() == 0
-            or self.__right_back_wheel_motor.get_subscription_count() == 0
-            or self.__right_middle_wheel_motor.get_subscription_count() == 0
-            or self.__right_front_wheel_motor.get_subscription_count() == 0
-        ):
-            llogger.debug("won't send speeds to sim until motor bridge is up")
-            sleep(0.25)
-
-        # set left wheel speeds
-        _ = self.get_logger().debug(f"left wheel speed: {left_wheel_speed}")
-        self.__left_front_wheel_motor.publish(left_msg)
-        self.__left_middle_wheel_motor.publish(left_msg)
-        self.__left_back_wheel_motor.publish(left_msg)
-
-        # set right wheel speeds
-        _ = self.get_logger().debug(f"right wheel speed: {right_wheel_speed}")
-        self.__right_front_wheel_motor.publish(right_msg)
-        self.__right_middle_wheel_motor.publish(right_msg)
-        self.__right_back_wheel_motor.publish(right_msg)
 
 
 def main(args: list[str] | None = None):
