46b3b80749c14717afe30eb62cc7783ece699fba
Barrett Ray <contact@barretts.club>
2025-05-08 20:55:12 -0500

    fix(drive/launch/nav2): fix param percolation
    they won't go into the container unless it's defined this way :p

--- src/drive_launcher/launch/helpers/nav2.launch.py
+++ src/drive_launcher/launch/helpers/nav2.launch.py
@@ -5,19 +5,28 @@ from launch.substitutions import (
     LaunchConfiguration,
     PathJoinSubstitution,
 )
-from launch_ros.actions import LoadComposableNodes, SetParameter
+from launch_ros.actions import (
+    ComposableNodeContainer,
+    LoadComposableNodes,
+)
 from launch_ros.descriptions import ComposableNode
+from nav2_common.launch import RewrittenYaml
+
+CONTAINER_NAME: str = "nav2_container"
+"""The name of the Nav2 container we're starting."""
 
 
 def generate_launch_description() -> LaunchDescription:
     # whether to use the simulator's time instead of ROS 2
-    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time: LaunchConfiguration = LaunchConfiguration("use_sim_time")
 
-    # TODO(bray): got not clue what these do lol
-    autostart = LaunchConfiguration("autostart")
-    container_name = LaunchConfiguration("container_name")
-    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+    # remap tf2 stuff to work within the container
+    remappings: list[tuple[str, str]] = [
+        ("/tf", "tf"),
+        ("/tf_static", "tf_static"),
+    ]
 
+    # grab our (yaml) configuration file for Nav2
     pkg_drive_launcher: str = get_package_share_directory("drive_launcher")
     params_file: PathJoinSubstitution = PathJoinSubstitution(
         [
@@ -27,106 +36,132 @@ def generate_launch_description() -> LaunchDescription:
         ]
     )
 
-    lifecycle_nodes: list[str] = [
-        "controller_server",
-        "smoother_server",
-        "planner_server",
-        "behavior_server",
-        "velocity_smoother",
-        "collision_monitor",
-        "bt_navigator",
-        "waypoint_follower",
-    ]
-
-    # we'll start all these nodes using "composition"
+    # screw with our yaml to make it work with composition.
     #
-    # for more info, see:
+    # note: there's a damn good reason this comes with Nav2! i have an entire
+    # library of GitHub Issues tickets complaining about the setup. lmao
     #
-    # https://docs.ros.org/en/humble/Concepts/Intermediate/About-Composition.html
-    composable_nodes = [
-        SetParameter("use_sim_time", use_sim_time),
-        LoadComposableNodes(
-            target_container=container_name,
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="nav2_controller",
-                    plugin="nav2_controller::ControllerServer",
-                    name="controller_server",
-                    parameters=[params_file],
-                    remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
-                ),
-                ComposableNode(
-                    package="nav2_smoother",
-                    plugin="nav2_smoother::SmootherServer",
-                    name="smoother_server",
-                    parameters=[params_file],
-                    remappings=remappings,
-                ),
-                ComposableNode(
-                    package="nav2_planner",
-                    plugin="nav2_planner::PlannerServer",
-                    name="planner_server",
-                    parameters=[params_file],
-                    remappings=remappings,
-                ),
-                ComposableNode(
-                    package="nav2_behaviors",
-                    plugin="behavior_server::BehaviorServer",
-                    name="behavior_server",
-                    parameters=[params_file],
-                    remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
-                ),
-                ComposableNode(
-                    package="nav2_bt_navigator",
-                    plugin="nav2_bt_navigator::BtNavigator",
-                    name="bt_navigator",
-                    parameters=[params_file],
-                    remappings=remappings,
-                ),
-                ComposableNode(
-                    package="nav2_waypoint_follower",
-                    plugin="nav2_waypoint_follower::WaypointFollower",
-                    name="waypoint_follower",
-                    parameters=[params_file],
-                    remappings=remappings,
-                ),
-                ComposableNode(
-                    package="nav2_velocity_smoother",
-                    plugin="nav2_velocity_smoother::VelocitySmoother",
-                    name="velocity_smoother",
-                    parameters=[params_file],
-                    remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
-                ),
-                ComposableNode(
-                    package="nav2_collision_monitor",
-                    plugin="nav2_collision_monitor::CollisionMonitor",
-                    name="collision_monitor",
-                    parameters=[params_file],
-                    remappings=remappings,
-                ),
-                ComposableNode(
-                    package="nav2_lifecycle_manager",
-                    plugin="nav2_lifecycle_manager::LifecycleManager",
-                    name="lifecycle_manager_navigation",
-                    parameters=[
-                        {
-                            "autostart": autostart,
-                            "node_names": lifecycle_nodes,
-                        }
-                    ],
-                ),
-            ],
-        ),
-    ]
-
-    ld: LaunchDescription = LaunchDescription()
-    for n in composable_nodes:
-        ld.add_action(n)
-
-    ld.add_action(
-        DeclareLaunchArgument(
-            "use_sim_time",
-            default_value="false",
-        ),
+    # - composition has trouble with params: https://github.com/ros-navigation/navigation2/issues/4011
+    # - nav2 regression on Humble + Jazzy: https://github.com/turtlebot/turtlebot4/discussions/550
+    # - nav2 launches the `local_costmap` and `global_costmap` on their own,
+    #       but attempting to do so yourself SUCKS: https://github.com/ros-navigation/navigation2/issues/2828
+    # - as i said above... https://github.com/ros-navigation/navigation2/blob/humble/nav2_controller/src/controller_server.cpp#L65
+    # - to conf costmaps in container, need to pass params to the container
+    #       itself: https://github.com/ros-navigation/navigation2/issues/4011
+    configured_yaml: RewrittenYaml = RewrittenYaml(
+        source_file=params_file,
+        root_key=None,  # required to avoid overlapping namespace
+        param_rewrites={"use_sim_time": use_sim_time},
+        convert_types=True,  # parse yaml types into literals
     )
-    return ld
+
+    # we need to make a "container" for the nodes to exist in. this container
+    # MUST be defined inside this file - otherwise, we can't remap the params
+    # correctly :(
+    #
+    # using a container allows nodes to share memory - node composition on Nav2
+    # usually speeds things up.
+    #
+    # see: https://docs.ros.org/en/humble/Tutorials/Intermediate/Composition.html#component-container-types
+    nav2_container: ComposableNodeContainer = ComposableNodeContainer(
+        name=CONTAINER_NAME,
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",  # note: `_mt` means we use one executor
+        parameters=[configured_yaml],  # IMPORTANT! params FAIL w/o it...
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+    )
+
+    # grab our list of lifecycle nodes
+    lifecycle_nodes: list[ComposableNode] = [
+        ComposableNode(
+            package="nav2_controller",
+            plugin="nav2_controller::ControllerServer",
+            name="controller_server",
+            parameters=[configured_yaml],
+            remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+        ),
+        ComposableNode(
+            package="nav2_smoother",
+            plugin="nav2_smoother::SmootherServer",
+            name="smoother_server",
+            parameters=[configured_yaml],
+            remappings=remappings,
+        ),
+        ComposableNode(
+            package="nav2_planner",
+            plugin="nav2_planner::PlannerServer",
+            name="planner_server",
+            parameters=[configured_yaml],
+            remappings=remappings,
+        ),
+        ComposableNode(
+            package="nav2_behaviors",
+            plugin="behavior_server::BehaviorServer",
+            name="behavior_server",
+            parameters=[configured_yaml],
+            remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+        ),
+        ComposableNode(
+            package="nav2_bt_navigator",
+            plugin="nav2_bt_navigator::BtNavigator",
+            name="bt_navigator",
+            parameters=[configured_yaml],
+            remappings=remappings,
+        ),
+        ComposableNode(
+            package="nav2_waypoint_follower",
+            plugin="nav2_waypoint_follower::WaypointFollower",
+            name="waypoint_follower",
+            parameters=[configured_yaml],
+            remappings=remappings,
+        ),
+        ComposableNode(
+            package="nav2_velocity_smoother",
+            plugin="nav2_velocity_smoother::VelocitySmoother",
+            name="velocity_smoother",
+            parameters=[configured_yaml],
+            remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+        ),
+        ComposableNode(
+            package="nav2_collision_monitor",
+            plugin="nav2_collision_monitor::CollisionMonitor",
+            name="collision_monitor",
+            parameters=[configured_yaml],
+            remappings=remappings,
+        ),
+    ]
+
+    # make the lifecycle manager to support the nodes above after loading.
+    #
+    # we'll spawn it first.
+    lifecycle_manager: ComposableNode = ComposableNode(
+        package="nav2_lifecycle_manager",
+        plugin="nav2_lifecycle_manager::LifecycleManager",
+        name="lifecycle_manager_navigation",
+        parameters=[  # two parameter sets:
+            {
+                "autostart": True,
+                "node_names": [n.node_name for n in lifecycle_nodes],
+            },
+            configured_yaml,
+        ],  # plus all common params
+        remappings=remappings,
+    )
+
+    # now, we'll load nodes in the container only after the server can recv.
+    # them. (this action blocks until the container is ready!)
+    #
+    # without this, we can't pass our config (via parameters) correctly.
+    load_composable_nodes: LoadComposableNodes = LoadComposableNodes(
+        target_container=CONTAINER_NAME,
+        composable_node_descriptions=lifecycle_nodes + [lifecycle_manager],
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("use_sim_time", default_value="false"),
+            nav2_container,
+            load_composable_nodes,
+        ]
+    )
---
+++ src/navigator/navigation_bringup_node/main.py
@@ -0,0 +1,141 @@
+import sys
+from dataclasses import dataclass
+from time import sleep
+
+import rclpy
+from geometry_msgs.msg import Twist
+from loguru import logger as llogger
+from nav_msgs.msg import OccupancyGrid
+from rclpy.node import Node, Publisher, Subscription
+from rclpy.qos import QoSPresetProfiles, QoSProfile
+from rclpy.timer import Timer
+from typing_extensions import override
+
+# the primary qos here is reliable - maps are sent reliably.
+QOS_PROFILE: QoSProfile = QoSPresetProfiles.SERVICES_DEFAULT.value
+
+MOVEMENT_SPEED: float = -0.05
+"""How fast we'll move backward."""
+
+
+@dataclass(kw_only=True)
+class NavigationBringupNode(Node):
+    _map_topic_subscriber: Subscription
+    """Subscribes to the `/map` topic frame representation."""
+
+    _wheels_publisher: Publisher
+    """
+    Publishes (very) slow speeds to the wheels for slight movement.
+
+    This gives the `slam_toolbox` nodes enough information to compelte their
+    `map -> odom -> base_link` chain!
+    """
+
+    _wheel_speeds_timer: Timer
+    """A timer that says when to stop/start moving the wheels."""
+
+    _is_done: bool = False
+    """
+    Indicates whether we've gotten a 'good' map.
+    """
+
+    def __init__(self):
+        super().__init__("navigation_bringup_node")
+        _ = self.get_logger().info("Performing navigation stack bringup...")
+
+        # make a publisher to write to the wheels
+        self._wheels_publisher = self.create_publisher(
+            Twist, "/cmd_vel", QOS_PROFILE
+        )
+        _ = self.get_logger().debug("Created wheel publisher.")
+
+        # on a timer, actually perform those publishes
+        self._wheel_speeds_timer = self.create_timer(
+            0.1,  # triggers every n seconds
+            self._on_wheel_speeds_timer_tick,
+        )
+        _ = self.get_logger().debug("Made wheel publishing timer.")
+
+        # wait for map to be not zero-sized
+        self._map_topic_subscriber = self.create_subscription(
+            OccupancyGrid, "/map", self._on_map_message, QOS_PROFILE
+        )
+        _ = self.get_logger().debug("Made map topic subscriber.")
+
+    # all ROS 2 nodes must be hashable!
+    @override
+    def __hash__(self) -> int:
+        return super().__hash__()
+
+    def _on_map_message(self, map_message: OccupancyGrid):
+        """
+        Waits until the `slam_toolbox` publish a map that's not zero-sized.
+        """
+
+        # if the map has a size over 0x0...
+        if (
+            map_message.info.width > 0
+            and map_message.info.height > 0
+            and not self._is_done
+        ):
+            # stop this node!
+            _ = self.get_logger().info(
+                f"`/map` is now sized ({map_message.info.width}x{map_message.info.height})! Allowing dependent nodes to start..."
+            )
+            self.stop_wheels()
+            self._wheel_speeds_timer.cancel()
+
+            # say that we're 'done'
+            self._is_done = True
+            raise SystemExit()
+
+    def _on_wheel_speeds_timer_tick(self):
+        # decide which speeds to publish
+        wheel_speed: Twist = Twist()
+        wheel_speed.linear.x = MOVEMENT_SPEED
+
+        # ...publish them.
+        self._wheels_publisher.publish(wheel_speed)
+
+    def stop_wheels(self):
+        if rclpy.ok():
+            # an empty message is 0 speed...
+            _ = self.get_logger().info("Stopping wheels...")
+            zero_speed_msg: Twist = Twist()
+
+            # publish it to stop the wheels
+            self._wheels_publisher.publish(zero_speed_msg)
+            _ = self.get_logger().info("Wheels stopped.")
+
+
+def main(args: list[str] | None = None):
+    llogger.info("Starting Navigation bringup...")
+    rclpy.init(args=args)
+    node: NavigationBringupNode = NavigationBringupNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        llogger.info("Ctrl^C detected. Shutting down gracefully...")
+    finally:
+        # stop the wheels when we're done.
+        #
+        # then, turn off `rclpy` connection
+        stop(node)
+
+
+def stop(node: NavigationBringupNode):
+    llogger.info("Calling `stop_wheels`...")
+    node.stop_wheels()
+    sleep(0.2)
+
+    llogger.info("Destroying node. Goodbye!")
+    node.destroy_node()
+
+    rclpy.shutdown()
+    llogger.info("Exiting...")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
