cc378007f7f1aacdf253e20584f6679792b84200
Barrett Ray <contact@barretts.club>
2025-05-24 11:02:47 -0500

    feat(lights): re-implement service server

--- src/lights/src/main.rs
+++ src/lights/src/main.rs
@@ -12,18 +12,22 @@
 use std::{
     net::{IpAddr, Ipv4Addr},
     sync::Arc,
+    time::Duration,
 };
 
-use custom_interfaces::srv::Lights;
+use custom_interfaces::srv::{Lights, Lights_Request, Lights_Response};
 
 use feedback::{prelude::RoverController, Led};
 
-use ros2_client::Server;
 use safe_drive::{
     context::Context,
     logger::Logger,
     node::{Node, NodeOptions},
-    pr_error, pr_info, qos,
+    pr_error, pr_fatal, pr_info, qos,
+    service::{
+        server::{Server, ServerSend},
+        Header,
+    },
 };
 
 const LIGHTS_NODE_NAME: &str = "lights_node";
@@ -39,7 +43,7 @@ async fn main() {
     let logger: Arc<Logger> = Arc::new(Logger::new(LIGHTS_NODE_NAME));
 
     // Create the lights node
-    let mut node: Arc<Node> = ctx
+    let node: Arc<Node> = ctx
         .create_node(LIGHTS_NODE_NAME, None, NodeOptions::new())
         .inspect_err(|e| pr_error!(logger, "Failed to create node: {e}"))
         .inspect(|_| {
@@ -65,47 +69,81 @@ async fn main() {
         .expect("Failed to create Rover Controller");
 
     // Handle requests in the background
-    let next = tokio::select! {
+    tokio::select! {
         _ = tokio::signal::ctrl_c() => (),
         _ = service_server_task(server, controller, Arc::clone(&logger)) => (),
     };
 }
 
 async fn service_server_task(
-    server: Server<Lights>,
+    mut server: Server<Lights>,
     controller: RoverController,
     logger: Arc<Logger>,
 ) {
     // wait for requests
     loop {
-        let maybe_request = server.async_receive_request().await;
+        let maybe_request: Result<(ServerSend<_>, Lights_Request, Header), _> = server.recv().await;
 
-        let (id, request) = match maybe_request {
-            Ok(a) => a,
+        // we got a request! let's unwrap what it contains...
+        let (responder, request, _header) = match maybe_request {
+            // if there's no error, just steal its contents
+            Ok(tup) => tup,
+
+            // but if we get an error, report it, then wait for a new message
             Err(e) => {
-                pr_error!(logger, "Failed to get message from request! err: {e}");
-                continue;
+                pr_fatal!(logger, "Failed to get message from request! err: {e}");
+
+                // FIXME: would prefer this is `continue`, but we don't get
+                // back a handle to the `server` in the error... D:
+                return;
             }
         };
 
-        // TODO: handle lights request
+        // make a new RGB value with the request's color
         let led = Led {
-            red: 255,
-            green: 0,
-            blue: 0,
+            red: request.red,
+            green: request.green,
+            blue: request.blue,
         };
 
         // try sending the LED color to the microcontroller
-        if let Err(e) = controller.send_led(&led).await {
-            pr_error!(logger, "Failed to send light color to Ebox! err: {e}");
-        }
+        let success = match controller.send_led(&led).await {
+            Ok(_) => true,
+            Err(e) => {
+                pr_error!(logger, "Failed to send light color to Ebox! err: {e}");
+                false
+            }
+        };
 
-        // This is what is sent back to the client (returns true if successful, false if not)
-        // let mut response = LightsResponse { success: false };
+        // send a response back based on how that went...
+        //
+        // (returns true if successful, false if not)
+        let response = Lights_Response { success };
 
-        // send a response back based on how that went
-        let response = Lights::Response::new();
-        server.async_send_response(id, response);
+        // we'll try to send back the response.
+        //
+        // this can fail if `rcl` loses connection, so we have to `match on
+        // the result of this operation to "get the server back" - it was
+        // "consumed" by the request.
+        //
+        // in other words, we can only get back a server if we still have a
+        // valid `rcl` connection
+        let mut responder = responder;
+        server = loop {
+            match responder.send(&response) {
+                Ok(s) => break s,
+                Err((retry, e)) => {
+                    pr_fatal!(
+                    logger,
+                    "`rcl` connection error detected! Waiting 1.0s before trying again. err: {e}"
+                );
+
+                    // retry after a moment
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    responder = retry;
+                }
+            }
+        };
     }
 }
 
