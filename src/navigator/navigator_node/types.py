92ac1140108fda9c274a0b95c6d6bb29f4e3e2dc
Barrett Ray <contact@barretts.club>
2025-05-27 15:12:38 -0500

    feat(navigator/node): add aruco marker id field

--- src/navigator/navigator_node/types.py
+++ src/navigator/navigator_node/types.py
@@ -27,7 +27,15 @@ class NavigationParameters:
     coord: GeoPoint
     """
     The coordinate that either is our goal (if `mode` is `GPS`), or is some
-    point around an ArUco/object.
+    point around an ArUco marker or an object.
+    """
+
+    aruco_marker_id: int | None = None
+    """
+    We're searching for a marker with this ID.
+
+    ArUco markers have IDs defined by a dictionary. URC uses 4x4_50.
+    https://docs.opencv.org/3.4/d9/d6a/group__aruco.html#ggac84398a9ed9dd01306592dd616c2c975ada8e830ff0024e839e93c01f5fed0c55
     """
 
     mode: NavigationMode
