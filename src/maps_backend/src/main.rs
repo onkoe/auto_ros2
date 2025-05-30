e6dfb19fe090c3153f0be8465108bf66d1cfa96d
Barrett Ray <contact@barretts.club>
2025-05-29 20:53:54 -0600

    feat(maps): use `tracing` for better logging

--- src/maps_backend/src/main.rs
+++ src/maps_backend/src/main.rs
@@ -15,20 +15,32 @@ use tungstenite::Message;
 const MAPS_NODE: &str = "maps_node";
 
 #[tokio::main]
+#[tracing::instrument]
 async fn main() {
+    // start `tracing` crate logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     // Set up ROS2 context and node
+    tracing::debug!("Creating ROS 2 node...");
     let ctx: Arc<Context> = Context::new().unwrap();
     let node: Arc<Node> = ctx
         .create_node(MAPS_NODE, Some("/"), Default::default())
         .unwrap();
+    tracing::debug!("ROS 2 node created!");
 
+    tracing::debug!("Creating GPS subscriber...");
     // Set up GPS subscriber
     let mut gps_subscriber = node
         .create_subscriber::<NavSatFix>("/sensors/gps", Some(Profile::sensor_data()))
         .unwrap();
+    tracing::debug!("GPS subscriber created!");
 
     // Set up broadcast channel to share GPS data with WebSocket clients
-    let (tx, _rx) = broadcast::channel::<String>(16); // 16 is the buffer size
+    tracing::debug!("Making thread broadcast channel...");
+    let (tx, rx) = broadcast::channel::<String>(16); // 16 is the buffer size
+    tracing::debug!("Completed creating thread broadcast channel!");
 
     // Spawn WebSocket server in background
     tokio::spawn(start_websocket_server(tx.clone()));
@@ -47,9 +59,11 @@ async fn main() {
 }
 
 /// Handles WebSocket connections and broadcasts GPS data to each connected client.
-async fn start_websocket_server(tx: broadcast::Sender<String>) {
-    let listener = TcpListener::bind("127.0.0.1:9001").await.unwrap();
-    println!("✅ WebSocket server running at ws://localhost:9001");
+#[tracing::instrument]
+async fn start_websocket_server(mut rx: broadcast::Receiver<String>) {
+    tracing::debug!("Before TCP bind...");
+    let listener = TcpListener::bind(BIND_ADDR).await.unwrap();
+    tracing::info!("WebSocket server now running at ws://localhost:9001");
 
     loop {
         let (stream, _) = listener.accept().await.unwrap();
