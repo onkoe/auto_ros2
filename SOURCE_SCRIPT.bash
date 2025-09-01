a738d8982f86c0aeb1b9472d801c79756b267bf7
barrett <onkoe@protonmail.com>
2025-09-01 12:29:07 -0500

    fix(SOURCE_SCRIPT): remove old environment steps
    - uv: PyPi dependencies are now handled by pixi
    - ros2: pixi automatically sources the ROS 2 env on `pixi shell`

--- SOURCE_SCRIPT.bash
+++ SOURCE_SCRIPT.bash
@@ -4,22 +4,12 @@ set -e
 GREEN="\e[32m"
 RESET="\e[0m"
 
-# ros variables
-ROS_DISTRO="$(cat .ros_distro)"
-ROS_PATH="$(cat .ros_path)$ROS_DISTRO"
-
 echo -n "You should be running this script using \`" && echo -n "$GREEN. ./SOURCE_SCRIPT.bash$RESET" && echo "\`."
 echo "If you didn't do so, please restart the script."
 echo
 
-echo "Sourcing virtual environment..."
-. .venv/bin/activate
-echo "Done!"
-echo
-
 echo "Sourcing ROS 2 environment files..."
 . install/local_setup.bash || true
-. $ROS_PATH/setup.bash || true
 echo "Done!"
 echo
 
--- SOURCE_SCRIPT.fish
+++ SOURCE_SCRIPT.fish
@@ -2,22 +2,12 @@
 
 set ROS2_WORKSPACE_LOCATION (dirname (readlink -m (status --current-filename)))
 
-# grab ros 2 properties
-set ROS_DISTRO (cat .ros_distro)
-set ROS_PATH (cat .ros_path)$ROS_DISTRO
-
 echo -n "You should be running this script using `" && set_color green && echo -n ". ./SOURCE_SCRIPT.fish" && set_color normal && echo "`."
 echo "If you didn't do so, please restart the script."
 echo
 
-echo "Sourcing virtual environment..."
-. .venv/bin/activate.fish
-echo "Done!"
-echo
-
 echo "Sourcing ROS 2 environment files..."
 bass source $ROS2_WORKSPACE_LOCATION/install/local_setup.bash || true
-bass source $ROS_PATH/setup.bash || true
 echo "Done!"
 echo
 
